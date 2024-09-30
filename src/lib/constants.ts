9de568d30a45b9c54b7a2c2e614fb7e3e899d1c1
Luka <lukabrkovic@proton.me>
2024-09-30 19:02:17 +0200

    feat: temp docker build

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
-export const CLIENT_BASE_URL = 'http://152.53.19.179:8070/v1';
-export const SERVER_BASE_URL = 'http://localhost:8070/v1';
+export const CLIENT_BASE_URL = 'https://api.scylla-flow.com/v1';
+export const SERVER_BASE_URL = 'http://api:8070/v1';
 export const ACCESS_TOKEN = 'access_token';
 export const REFRESH_TOKEN = 'refresh_token';
 export const COGNITO_ID = 'cognito_id';
-export const isProduction = false;
+export const isProduction = true;
